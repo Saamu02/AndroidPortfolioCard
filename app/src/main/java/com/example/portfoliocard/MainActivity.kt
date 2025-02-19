5621fd06d2aacc8c7565690d563868a395bef2b4
Saamu02 <iusamairfan@gmail.com>
2025-02-20 01:26:49 +0500

    Designed the profile image view

--- app/src/main/java/com/example/portfoliocard/MainActivity.kt
+++ app/src/main/java/com/example/portfoliocard/MainActivity.kt
@@ -4,13 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.portfoliocard.ui.theme.PortfolioCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +46,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             PortfolioCardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    CreatePortfolioCard()
+                    CreatePortfolioCard(
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
@@ -28,14 +56,68 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CreatePortfolioCard() {
+fun CreatePortfolioCard(modifier: Modifier = Modifier) {
 
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .safeDrawingPadding()
+    ) {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(15.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            colors = CardDefaults.cardColors(Color.White),
+            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+        ) {
+
+            Column(
+                modifier = Modifier,
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                ProfileImage()
+                
+                HorizontalDivider(
+                    modifier = Modifier
+                        .padding(top = 10.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun ProfileImage(modifier: Modifier = Modifier) {
+
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp)
+            .padding(top = 5.dp),
+        shape = CircleShape,
+        border = BorderStroke(
+            width = 0.5.dp,
+            Color.LightGray
+        ),
+        shadowElevation = 4.dp,
+        color = MaterialTheme.colorScheme.surface.copy(alpha = 0.5f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_image),
+            contentDescription = "profile image",
+            modifier = Modifier.size(130.dp),
+            contentScale = ContentScale.Crop
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PortfolioCardTheme {
-
+        CreatePortfolioCard()
     }
 }
